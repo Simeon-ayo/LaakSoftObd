a066f61b768a184699e43828f66dba1784ca2e27
Henk van der Laak <henk@laaksoft.nl>
2013-08-18 15:17:10 +0200

    Reorder obddata

--- src/nl/laaksoft/obd/reader/ObdData.java
+++ src/nl/laaksoft/obd/reader/ObdData.java
@@ -2,12 +2,15 @@ package nl.laaksoft.obd.reader;
 
 public class ObdData
 {
-    public double m_EngineLoad;
-    public double m_CoolingTemperature;
+    public double m_VehicleSpeed; // 1 1
+    public double m_EngineRpm; // 1 2
+
+    public double m_EngineLoad; // 2 1
+    public double m_CoolingTemperature; // 2 2
+
+    public double m_RailPressure; // 3 1
+    public double m_MafRate; // 3 2
+
     public double m_IntakePressure;
-    public double m_EngineRpm;
-    public double m_RailPressure;
-    public double m_MafRate;
     public double m_IntakeTemperature;
-    public double m_VehicleSpeed;
 }
