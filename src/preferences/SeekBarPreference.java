4d5148c661a8641bd8de6ceb8ded3fe7530f402c
Henk van der Laak <henk@laaksoft.nl>
2013-09-15 14:54:56 +0200

    Remove unused libs

--- src/preferences/SeekBarPreference.java
+++ src/preferences/SeekBarPreference.java
@@ -10,58 +10,52 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class SeekBarPreference extends Preference implements OnSeekBarChangeListener
-{
-    private TextView tvValue;
-    private final int DEFAULT_SEEKBAR_POSITION = 0;
+public class SeekBarPreference extends Preference implements
+		OnSeekBarChangeListener {
+	private TextView tvValue;
+	private final int DEFAULT_SEEKBAR_POSITION = 0;
 
-    public SeekBarPreference(Context context)
-    {
-        super(context);
-    }
+	public SeekBarPreference(Context context) {
+		super(context);
+	}
 
-    public SeekBarPreference(Context context, AttributeSet attrs)
-    {
-        super(context, attrs);
-    }
+	public SeekBarPreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
 
-    public SeekBarPreference(Context context, AttributeSet attrs, int defStyle)
-    {
-        super(context, attrs, defStyle);
-    }
+	public SeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
 
-    @Override
-    protected void onBindView(View view)
-    {
-        tvValue = (TextView) view.findViewById(R.id.tvSbPref);
-        SeekBar sbValue = (SeekBar) view.findViewById(R.id.sbSbPref);
+	@Override
+	protected void onBindView(View view) {
+		tvValue = (TextView) view.findViewById(R.id.tvSbPref);
+		SeekBar sbValue = (SeekBar) view.findViewById(R.id.sbSbPref);
 
-        sbValue.setOnSeekBarChangeListener(this);
+		sbValue.setOnSeekBarChangeListener(this);
 
-        SharedPreferences sp = getSharedPreferences();
-        int value = sp.getInt(getKey(), DEFAULT_SEEKBAR_POSITION);
-        sbValue.setProgress(value);
-        tvValue.setText(Integer.valueOf(value).toString());
+		SharedPreferences sp = getSharedPreferences();
+		int value = sp.getInt(getKey(), DEFAULT_SEEKBAR_POSITION);
+		sbValue.setProgress(value);
+		tvValue.setText(Integer.valueOf(value).toString());
 
-        super.onBindView(view);
-    }
+		super.onBindView(view);
+	}
 
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
-    {
-        String tmp = Integer.valueOf(progress).toString();
-        tvValue.setText(tmp);
-    }
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+		String tmp = Integer.valueOf(progress).toString();
+		tvValue.setText(tmp);
+	}
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar)
-    {
-    }
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+	}
 
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar)
-    {
-        tvValue.setText(Integer.valueOf(seekBar.getProgress()).toString());
-        persistInt(seekBar.getProgress());
-    }
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		tvValue.setText(Integer.valueOf(seekBar.getProgress()).toString());
+		persistInt(seekBar.getProgress());
+	}
 }
